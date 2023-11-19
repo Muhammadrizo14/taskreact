9575028e8c7a64e2e59f83448d882168f77f9e11
Muhammadrizo <mtursunboev13@gmail.com>
2023-11-19 15:30:30 +0500

    deleteblockcontainer

--- src/page/country.js
+++ src/page/country.js
@@ -1,78 +1,88 @@
-import React, {useEffect, useState} from 'react';
-import {Link, useParams} from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import axios from "axios";
 
 const Country = () => {
-  const [country, setCountry] = useState({})
-  const [lived, setLived] = useState([])
-  const [burn, setBurn] = useState(1978)
-  const [death, setDeath] = useState(2000)
-  const {id} = useParams()
-
+  const [country, setCountry] = useState({});
+  const [lived, setLived] = useState([]);
+  const [burn, setBurn] = useState();
+  const [death, setDeath] = useState();
+  const { id } = useParams();
 
   const getAllCountries = () => {
     axios
       .get(`http://localhost:3000/country/${id}`)
-      .then(res => {
-        setCountry(res.data)
+      .then((res) => {
+        setCountry(res.data);
       })
-      .catch(err => {
-        console.log(err)
-      })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   function findPeopleByPeriod() {
-    const livingPeople = country.people.filter(person => {
+    const livingPeople = country.people.filter((person) => {
       if (person.death) {
         return person.burn <= death && person.death >= burn;
       } else {
         return person.burn <= burn;
       }
     });
-    setLived(livingPeople)
+    setLived(livingPeople);
   }
 
-
-
   useEffect(() => {
-    getAllCountries()
-  }, [])
+    getAllCountries();
+  }, []);
   return (
     <div>
-      <Link to={'/'}>back to home</Link>
-      <br/>
-      <br/>
-      <br/>
-
+      <Link to={"/"}>back to home</Link>
+      <br />
+      <br />
+      <br />
 
       <h1>{country.city}</h1>
 
       <div>
         <h1>Sort by period</h1>
-        <input type="number" value={burn} placeholder='start year' onChange={(e)=> setBurn(e.target.value)}/>
-        <input type="number" value={death} placeholder='end year' onChange={(e)=> setDeath(e.target.value)}/>
-        <button onClick={()=> findPeopleByPeriod()}>get</button>
+        <input
+          type="number"
+          value={burn}
+          placeholder="start year"
+          onChange={(e) => setBurn(e.target.value)}
+        />
+        <input
+          type="number"
+          value={death}
+          placeholder="end year"
+          onChange={(e) => setDeath(e.target.value)}
+        />
+        <button onClick={() => findPeopleByPeriod()}>get</button>
       </div>
 
-      <br/>
-      <br/>
-      <br/>
+      <br />
+      <br />
+      <br />
 
-      <div className='people'>
-        {!lived.length && country.people?.map(person => (
-          <div key={person.id} className='person'>
+      <div className="people">
+        {!lived.length &&
+          country.people?.map((person) => (
+            <div key={person.id} className="person">
+              <h1>{person.name}</h1>
+              <p>
+                {person.burn} {person.death ? `- ${person.death}` : "-"}
+              </p>
+            </div>
+          ))}
+
+        {lived?.map((person) => (
+          <div key={person.id} className="person">
             <h1>{person.name}</h1>
-            <p>{person.burn} {person.death ? `- ${person.death}` : '-'}</p>
+            <p>
+              {person.burn} {person.death ? `- ${person.death}` : "-"}
+            </p>
           </div>
         ))}
-
-        {lived?.map(person => (
-          <div key={person.id} className='person'>
-            <h1>{person.name}</h1>
-            <p>{person.burn} {person.death ? `- ${person.death}` : '-'}</p>
-          </div>
-        ))}
-
       </div>
     </div>
   );
